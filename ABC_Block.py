b95c747aea45e9fa3478574f535bb7786b4628b1
Tunlaya-Sanphokha <s6201012620341@email.kmutnb.ac.th>
2021-08-08 23:47:50 +0700

    Draw BoardGame

--- ABC_Block.py
+++ ABC_Block.py
@@ -1,8 +1,30 @@
-class Borad :
+class Board :
     def __init__(self):
+        self.board_array = [['A','B','C','D'],['F','E','G','H'],['I','K','J',' ']]
+
+    def display_board(self,):
+        print()
+        for i in self.board_array:
+            for j in i:
+                print("|",end='')
+                print(j,end="")
+            print("|\n---------")
+        print()
+    #def change_player(self):
+    
+
+
+    #def add_position(self): 
+   
+
+class InputProcessor:
+    pass
+
+
     
 
 
 
-class InputProcessor:
-    def __init__(self):
+
+board = Board()
+board.display_board()
