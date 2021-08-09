725f5f03b56cf372e020ea4b1393ae5dd10fe659
KhongdetSomchai <s6201012620040@email.kmutnb.ac.th>
2021-08-09 23:45:38 +0700

    ทำให้ตอนเรียกใช้อ็อบเจค ทำงานและแสดงบอร์ดออกมาได้ และ เพิ่มฟังก์ชั่น play_game() เข้าไป

--- ABC_Block.py
+++ ABC_Block.py
@@ -1,3 +1,4 @@
+
 class Board :
     def __init__(self):
         self.board_game1 = [['C','D','B','A'],['F','J','H','I'],['E','G','K',' ']]
@@ -6,7 +7,7 @@ class Board :
         self.board_game4 = [['B','A','E','D'],['F','C','G','I'],['J','K','H',' ']]
         
         self.board_game = [['','','',''],['','','',''],['','','',' ']]
-        print(self.board_game)
+
 
     def display_board(self,pos):
         print(pos[0][0] + '|' + pos[0][1] + '|' + pos[0][2] + '|' + pos[0][3])
@@ -14,6 +15,23 @@ class Board :
         print(pos[1][0] + '|' + pos[1][1] + '|' + pos[1][2] + '|' + pos[1][3])
         print('-------')
         print(pos[2][0] + '|' + pos[2][1] + '|' + pos[2][2] + '|' + pos[2][3])
+
+    def play_game(self) :
+        chosse_board = int(input("you want to play borad 1 or 2 or 3 or 4: "))
+        if chosse_board == 1:
+            self.board_game = self.board_game1
+            self.display_board(self.board_game)
+        elif chosse_board == 2:
+            self.board_game = self.board_game2
+            self.display_board(self.board_game)
+        elif chosse_board == 3:
+            self.board_game = self.board_game3
+            self.display_board(self.board_game)
+        elif chosse_board == 4:
+            self.board_game = self.board_game4
+            self.display_board(self.board_game)
+        new_position = InputProcessor.add_input(self)
+
     
     def select_character(self,board_game,new_input): 
         pass   
@@ -27,8 +45,8 @@ class Board :
 
 class InputProcessor:
     def add_input(self) :
-    pos = input("which one do you want to switch ---> ")
-    return pos
+        pos = input("select you want to switch :  ")
+        return pos
     pass
 
 
@@ -36,4 +54,4 @@ class InputProcessor:
 
 
 board = Board()
-board.display_board()
+board.play_game()
